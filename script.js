19456a7d2e6b505c289ad0c3695596ebb90bcd6e
thalles45137 <135254987+thalles45137@users.noreply.github.com>
2025-03-27 12:10:08 -0300

    h

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ function draw() {
 
     // Pontuação
     ctx.font = "20px Arial";
-    ctx.fillText(playerScore, canvas.width / 4, 7);
+    ctx.fillText(playerScore, canvas.width / 4, 11);
     ctx.fillText(aiScore, (canvas.width / 4) * 3, 30);
 }
 
